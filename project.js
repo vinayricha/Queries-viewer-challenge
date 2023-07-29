a84e99fd862a47134f731266834960475fa825a8
Vinay <vinaychary8614@gmail.com>
2023-07-29 22:47:45 +0530

    First commit

---
+++ project.js
@@ -0,0 +1,12 @@
+// variables
+
+const container = document.getElementsByClassName('content-container')
+;
+
+
+for( i = 0; i < container.length; i++) {
+
+    container[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+    })
+};
